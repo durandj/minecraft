bc9d3c1182b6c496e733f036d9591d476721cf53
James Durand <james.durand@alumni.msoe.edu>
2014-04-04 13:53:06 -0500

    Show updates on home page

--- minecraft/app/views.py
+++ minecraft/app/views.py
@@ -4,10 +4,24 @@ Minecraft app views
 
 from django.views import generic as django_views
 
+from minecraft.app import models as minecraft_models
+
 class HomeView(django_views.TemplateView):
 	"""
 	The home view
 	"""
 
 	template_name = 'home.html'
+
+	def get_context_data(self, **kargs):
+		"""
+		Add the update info to the context.
+		"""
+
+		context = {
+			'mod_updates': minecraft_models.Mod.objects.order_by('last_edited_by')[:5],
+			'modpack_updates': minecraft_models.Modpack.objects.order_by('last_edited_by')[:5],
+		}
+
+		return super(HomeView, self).get_context_data(**context)
 
