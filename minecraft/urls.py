e40a6bcafcf75db6c70ac320c2c27f6d5bee514a
James Durand <james.durand@alumni.msoe.edu>
2014-04-03 20:43:29 -0500

    Initial commit

---
+++ minecraft/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'minecraft.views.home', name='home'),
+    # url(r'^minecraft/', include('minecraft.foo.urls')),
+)
