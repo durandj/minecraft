c68f040af328237c25fe818364324f225d261d4b
James Durand <james.durand@alumni.msoe.edu>
2014-04-05 12:09:27 -0500

    Convert mod/modpack relationship to a manytomany

--- minecraft/app/models.py
+++ minecraft/app/models.py
@@ -30,27 +30,13 @@ class Modpack(django_models.Model):
 	name        = django_models.CharField(max_length = 256, unique = True)
 	description = django_models.CharField(max_length = 1024)
 	url         = django_models.URLField()
+	mods        = django_models.ManyToManyField(Mod)
 
 	created_by     = django_models.ForeignKey(
 			auth_models.User, related_name = 'created_modpacks')
 	created_on     = django_models.DateTimeField(auto_now_add = True)
 	last_edited_by = django_models.ForeignKey(
 			auth_models.User, null = True, related_name = 'edited_modpacks')
-	last_edited_on = django_models.DateTimeField(auto_now = True, null = True)
-
-class ModpackMod(django_models.Model):
-	"""
-	A mod in a modpack
-	"""
-
-	mod     = django_models.ForeignKey(Mod)
-	modpack = django_models.ForeignKey(Modpack, related_name = 'mods')
-
-	created_by     = django_models.ForeignKey(
-			auth_models.User, related_name = 'created_modpack_mod')
-	created_on     = django_models.DateTimeField(auto_now_add = True)
-	last_edited_by = django_models.ForeignKey(
-			auth_models.User, null = True, related_name = 'edited_modpackmods')
 	last_edited_on = django_models.DateTimeField(auto_now = True, null = True)
 
 class Link(django_models.Model):
